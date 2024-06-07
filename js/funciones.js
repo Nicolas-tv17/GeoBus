b9c7199e8e199c48718d79d511f379ad44d75dd8
Sebastian Usma <johan58usma@gmail.com>
2024-06-06 21:45:00 -0500

    Correción en localización

--- js/funciones.js
+++ js/funciones.js
@@ -14,7 +14,7 @@ function obtenerUbicacionEnTiempoReal() {
 
             // Actualizar la URL del iframe de Google Maps con la ubicación en tiempo real
             let iframe = document.getElementById('mapa');
-            iframe.src = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d835.7943748171397!2d${longitud}!3d${latitud}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3887434840a9f9%3A0x5ddd4b4343acbc34!2sSede%20SENA%20CAISA%20(la%2020)!5e0!3m2!1ses-419!2sco!4v1716387179046!5m2!1ses-419!2sco`;
+            iframe.src = `https://www.google.com/maps?q=${latitud},${longitud}&z=15&output=embed`;
         },
         error => {
             console.error("Error al obtener la ubicación:", error);
