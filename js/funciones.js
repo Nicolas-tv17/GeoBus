df7aff0bc84e81226e9580bf7d88187cba602fd2
Sebastian Usma <johan58usma@gmail.com>
2024-06-15 19:32:25 -0500

    actualizar ubicacion

--- js/funciones.js
+++ js/funciones.js
@@ -1,29 +1,41 @@
 let p_latitud = document.getElementById('latitud');
 let p_longitud = document.getElementById('longitud');
+let iframe = document.getElementById('mapa');
 
-function obtenerUbicacionEnTiempoReal() {
-    // Establecer el observador para obtener la ubicación en tiempo real
-    let watcher = navigator.geolocation.watchPosition(
+function obtenerUbicacion() {
+    navigator.geolocation.getCurrentPosition(
         position => {
             // Obtener las coordenadas de la ubicación actual
             let latitud = position.coords.latitude;
             let longitud = position.coords.longitude;
 
-            p_latitud.textContent = latitud;
-            p_longitud.textContent =longitud;
+            
 
-            // Actualizar la URL del iframe de Google Maps con la ubicación en tiempo real
-            let iframe = document.getElementById('mapa');
-            iframe.src = `https://www.google.com/maps?q=${latitud},${longitud}&z=15&output=embed`;
+            if (p_latitud && p_longitud && iframe) {
+                p_latitud.textContent = latitud;
+                p_longitud.textContent = longitud;
+
+                // Actualizar la URL del iframe de Google Maps con la ubicación en tiempo real
+                console.log(latitud + ' ' + longitud);
+                iframe.src = `https://www.google.com/maps?q=${latitud},${longitud}&z=15&output=embed`;
+            } else {
+                console.error("No se encontraron los elementos DOM requeridos.");
+            }
         },
         error => {
             console.error("Error al obtener la ubicación:", error);
         }
     );
-    
-    // Si deseas detener el seguimiento de la ubicación en algún momento, puedes usar:
-    // navigator.geolocation.clearWatch(watcher);
+}
+
+function iniciarSeguimientoUbicacion() {
+    if (navigator.geolocation) {
+        // Llamar a obtenerUbicacion cada 2 segundos
+        setInterval(obtenerUbicacion, 5000);
+    } else {
+        console.error("La geolocalización no es soportada por este navegador.");
+    }
 }
 
 // Llamar a la función para comenzar a obtener la ubicación en tiempo real
-obtenerUbicacionEnTiempoReal();
+iniciarSeguimientoUbicacion();
